a56306bf04ff3f3e30c4a6565df8b9cd646f8f72
Govind Pande <117948813+govindpande@users.noreply.github.com>
2023-04-23 11:34:41 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import pandas_profiling
+import streamlit as st
+from streamlit_pandas_profiling import st_profile_report
+
+
+
+@st.cache
+def get_data():
+  # return pd.read_csv("http://data.insideairbnb.com/united-states/ny/new-york-city/2019-09-12/visualisations/listings.csv")
+  return pd.read_csv('data/energy.csv')
+
+
+
+
+def main():
+  df = get_data()
+
+  ################################ SIDEBAR ################################
+
+  st.sidebar.header("Final Project for MS")
+
+  st.sidebar.markdown(" ")
+  st.sidebar.markdown("*This project is done as a part of the final project for the MS in Data Science")
+
+  pr = df.profile_report()
+
+  st_profile_report(pr)
+
+
+main()
