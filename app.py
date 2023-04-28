3d67766313dd3ad2e1955e02f358499878b9b305
Govind Pande <117948813+govindpande@users.noreply.github.com>
2023-04-28 11:26:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,16 @@
-import pandas as pd
-import ydata_profiling
+import yfinance as yf
 import streamlit as st
-from streamlit_pandas_profiling import st_profile_report
 
+# Set up the sidebar options
+st.sidebar.title("Options Chain Viewer")
+ticker = st.sidebar.text_input("Enter ticker symbol (e.g. AAPL):", "AAPL")
+expiry_date = st.sidebar.date_input("Select expiry date:")
 
+# Fetch the option chain data
+ticker_data = yf.Ticker(ticker)
+option_chain = ticker_data.option_chain(expiry_date)
 
-@st.cache
-def get_data():
-  # return pd.read_csv("http://data.insideairbnb.com/united-states/ny/new-york-city/2019-09-12/visualisations/listings.csv")
-  return pd.read_csv('data/energy.csv')
-
-
-
-
-def main():
-  df = get_data()
-
-  ################################ SIDEBAR ################################
-
-  st.sidebar.header("Final Project for MS")
-
-  st.sidebar.markdown(" ")
-  st.sidebar.markdown("*This project is done as a part of the final project for the MS in Data Science")
-
-  pr = df.profile_report()
-
-  st_profile_report(pr)
-
-
-main()
+# Display the option chain in a table
+st.write(f"## {ticker} Options Chain for {expiry_date}")
+st.write(option_chain.calls)
+st.write(option_chain.puts)
